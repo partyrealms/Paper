290b26f97127127472dd8dd798a344e5fe51ba8e
partydev <admin@partyrealms.com>
2025-07-12 13:41:36 -0700

    Update adventure to 4.24.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         maven(paperMavenPublicUrl)
     }
 
